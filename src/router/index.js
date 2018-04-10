984371c01b6e512e2eedb5b0260422c5a9141334
DaveP <dave@wearecrystal.uk>
2018-04-10 19:48:25 +0100

    Some Vue shit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '@/components/Home'
+
+import Modifiers from '@/components/modifiers/Modifiers'
+import Modifier from '@/components/modifiers/Modifier'
+
+import Nouns from '@/components/nouns/Nouns'
+import Noun from '@/components/nouns/Noun'
 
 Vue.use(Router)
 
@@ -8,8 +15,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
+    },
+    {
+      path: '/modifiers',
+      component: Modifiers,
+      children: {
+        path: ':id',
+        component: Modifier
+      }
+    },
+    {
+      path: '/nouns',
+      component: Nouns,
+      children: {
+        path: ':id',
+        component: Noun
+      }
     }
   ]
 })
