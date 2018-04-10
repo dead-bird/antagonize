8edd71e184adb83ef73de913669e36a69df6cb72
DaveP <dave@wearecrystal.uk>
2018-04-10 23:47:12 +0100

    A home page?

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,12 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="container">
+    <h1>antagonize</h1>
+    <h4>ænˈtæɡəˌnaɪz</h4>
+    verb<br>
+    <strong>to make hostile; annoy or irritate.</strong><br>
+    "His criticism <em>antagonized</em> his friends"
   </div>
 </template>
 
