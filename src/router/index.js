6565349db49238f9b6de2799f2b65e3ca16ad2da
DaveP <dave@wearecrystal.uk>
2018-04-10 21:06:01 +0100

    airbnb linting

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import Modifiers from '@/components/modifiers/Modifiers';
+import Modifier from '@/components/modifiers/Modifier';
+import Nouns from '@/components/nouns/Nouns';
+import Noun from '@/components/nouns/Noun';
 
-import Home from '@/components/Home'
-
-import Modifiers from '@/components/modifiers/Modifiers'
-import Modifier from '@/components/modifiers/Modifier'
-
-import Nouns from '@/components/nouns/Nouns'
-import Noun from '@/components/nouns/Noun'
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
