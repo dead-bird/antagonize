7097d14caceecbb169dd351f6aba57e40b0caca4
DaveP <dave@wearecrystal.uk>
2018-04-11 23:13:12 +0100

    Catchall route

---
+++ src/components/single/Lost.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+};
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <h1>404 probably</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Modifiers from '@/components/modifiers/Modifiers';
 import Modifier from '@/components/modifiers/Modifier';
 import Nouns from '@/components/nouns/Nouns';
 import Noun from '@/components/nouns/Noun';
+import Lost from '@/components/single/Lost';
 
 Vue.use(Router);
 
@@ -31,6 +32,11 @@ export default new Router({
     {
       path: '/noun/:id',
       component: Noun,
+    },
+
+    {
+      path: '*',
+      component: Lost,
     },
   ],
 });
