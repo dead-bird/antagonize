020facf485366f763b6752b1e34b17390c004625
DaveP <dave@wearecrystal.uk>
2018-04-11 23:52:22 +0100

    Started Axios stuff

--- src/components/modifiers/Modifier.vue
+++ src/components/modifiers/Modifier.vue
@@ -1,11 +1,39 @@
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      modifiers: [],
+    };
+  },
+
+  created() {
+    this.api();
+  },
+
+  methods: {
+    api() {
+      const p = this.$route.params;
+      const api = process.env.API;
+      console.log(`${api}modifiers/${p.id}`);
+
+      axios.get(`${api}modifiers/${p.id}`).then((res) => {
+        this.modifiers = res;
+        console.log(res);
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div>
     <h1>Modifier: {{ $route.params.id }}</h1>
+
+    <pre>{{modifiers}}</pre>
   </div>
 </template>
 
