610dc941012f6a823722ad0078a7d3d238a5be33
DaveP <dave@wearecrystal.uk>
2018-04-11 23:39:14 +0100

    Vue Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
+import Home from '@/components/single/Home';
+import Lost from '@/components/single/Lost';
 import Modifiers from '@/components/modifiers/Modifiers';
 import Modifier from '@/components/modifiers/Modifier';
 import Nouns from '@/components/nouns/Nouns';
 import Noun from '@/components/nouns/Noun';
-import Lost from '@/components/single/Lost';
 
 Vue.use(Router);
 
