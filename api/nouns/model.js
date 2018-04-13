88530e58e280be137f30aa6d5a2ca5c527c15db4
DaveP <dave@wearecrystal.uk>
2018-04-13 23:59:11 +0100

    Mongoose Models? (i'm wingin' it)

---
+++ api/nouns/model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+let schema = new mongoose.Schema({
+  text: String,
+  nsfw: Boolean,
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Nouns', schema);
