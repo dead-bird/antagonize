e920ff24148d8a89cd64d32c4d0fe55db77562c6
DaveP <dave@wearecrystal.uk>
2018-04-14 12:50:39 +0100

    Shitty nav

--- src/components/parts/Head.vue
+++ src/components/parts/Head.vue
@@ -4,11 +4,19 @@ export default {
 </script>
 
 <template>
-  <header class="head"></header>
+  <header class="head">
+    <router-link class="menu-item" to="/">Dash</router-link>
+    <router-link class="menu-item" to="/modifiers">Modifiers</router-link>
+    <router-link class="menu-item" to="/nouns">Nouns</router-link>
+  </header>
 </template>
 
-<style lang="scss" scoped>
-// .head {
-//   padding: 60px 0;
-// }
+<style scoped>
+.head {
+  display: flex;
+}
+
+.menu-item {
+  padding: 5px;
+}
 </style>
