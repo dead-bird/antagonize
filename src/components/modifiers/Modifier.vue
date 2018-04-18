cb6723a7553ca368087f9be9457865efd2d1112a
DaveP <dave@wearecrystal.uk>
2018-04-18 23:45:11 +0100

    Started work on editting modifiers within Vue

--- src/components/modifiers/Modifier.vue
+++ src/components/modifiers/Modifier.vue
@@ -1,4 +1,6 @@
 <script>
+import api from '@/resources/base';
+
 export default {
   props: ['modifier'],
 
@@ -7,16 +9,44 @@ export default {
       // modifier: {},
     };
   },
+
+  methods: {
+    save() {
+      console.log(this.modifier._id);
+      console.log(this.modifier.nsfw);
+
+      // api.put(`modifier/${modifier.id}`)
+    },
+  },
 };
 </script>
 
 <template>
   <div>
-    <input type="text" :value="modifier.text">
+    <input class="modifier" type="text" :value="modifier.text" @keyup.enter="save">
+
+    <!-- <label class="nsfw-check" for="nsfw">️️⚠️</label> -->
+    <input name="nsfw" type="checkbox" :value="modifier.nsfw">
     <pre>{{ modifier }}</pre>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
+.modifier {
+  background-color: transparent;
+  color: inherit;
+  border: none;
+  font-size: 22px;
+  border-bottom: 1px solid white;
+  padding: 10px;
+}
 
+.modifier:active, .modifier:focus, .modifier.edit {
+  outline: none;
+  border-color: coral;
+}
+
+.nsfw-check {
+  display: inline-block;
+}
 </style>
