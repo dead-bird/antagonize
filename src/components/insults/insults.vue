62a3062e846f10eedb54fdbcdbbae9e93a0380dc
DaveP <dave@wearecrystal.uk>
2018-04-19 15:20:37 +0100

    Insults template

---
+++ src/components/insults/insults.vue
@@ -0,0 +1,26 @@
+<script>
+import Modifiers from '@/components/modifiers/Modifiers';
+import Nouns from '@/components/nouns/Nouns';
+
+export default {
+  components: { Modifiers, Nouns },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <Modifiers />
+      </div>
+
+      <div class="col-sm-6">
+        <Nouns />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
