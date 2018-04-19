3cdb63d7b529df0ab8c068fdb724382e812a33d1
DaveP <dave@wearecrystal.uk>
2018-04-19 17:33:02 +0100

    smol tweaks

--- src/components/parts/Head.vue
+++ src/components/parts/Head.vue
@@ -6,6 +6,7 @@ export default {
 <template>
   <header class="head">
     <router-link class="menu-item" to="/">Dash</router-link>
+    <router-link class="menu-item" to="/insults">Insults</router-link>
     <router-link class="menu-item" to="/modifiers">Modifiers</router-link>
     <router-link class="menu-item" to="/nouns">Nouns</router-link>
   </header>
