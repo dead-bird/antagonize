be4d3d575d387d54d40d18188ac64afc18144c4a
DaveP <dave@wearecrystal.uk>
2018-04-19 15:18:53 +0100

    Omit version key for now

--- api/modifiers/model.js
+++ api/modifiers/model.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-let schema = new mongoose.Schema({
-  text: String,
-  nsfw: Boolean,
-  date: { type: Date, default: Date.now },
-});
+let schema = new mongoose.Schema(
+  {
+    text: String,
+    nsfw: Boolean,
+    date: { type: Date, default: Date.now },
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('Modifiers', schema);
