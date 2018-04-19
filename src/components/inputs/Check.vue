e10274944a4d381b2078b56b55210f51f351dd17
DaveP <dave@wearecrystal.uk>
2018-04-19 17:33:59 +0100

    Check Component

---
+++ src/components/inputs/Check.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: ['nsfw'],
+};
+</script>
+
+<template>
+  <div class="nsfw" :class="nsfw ? 'checked' : ''" @click="$emit('nsfw')">️️⚠️</div>
+</template>
+
+<style scoped>
+.nsfw {
+  display: inline-block;
+  user-select: none;
+  font-size: 32px;
+  cursor: pointer;
+  line-height: 1;
+  margin-bottom: 0;
+  opacity: .25;
+  transition: .3s opacity;
+  padding-top: 6px;
+}
+
+.nsfw:hover, .nsfw.checked {
+  opacity: 1;
+}
+</style>
