42ef41f3571a241f0b6d5fa0e627560acc246216
DaveP <dave@wearecrystal.uk>
2018-04-20 21:37:20 +0100

    Change insult every X seconds

--- src/components/insults/Insult.vue
+++ src/components/insults/Insult.vue
@@ -4,26 +4,41 @@ import api from '@/resources/base';
 export default {
   data() {
     return {
-      insult: {
+      modifiers: {
         data: {},
       },
+
+      nouns: {
+        data: {},
+      },
+
+      m: 0,
+      n: 0,
     };
   },
 
   methods: {
     async fetch() {
-      this.insult = await api.get('insult');
+      this.modifiers = await api.get('modifiers');
+      this.nouns = await api.get('nouns');
+    },
+
+    next() {
+      this.m = Math.floor(Math.random() * this.modifiers.data.length);
+      this.n = Math.floor(Math.random() * this.nouns.data.length);
     },
   },
 
   created() {
     this.fetch();
+
+    setInterval(this.next, 5000);
   },
 };
 </script>
 
 <template>
-  <h1>you {{ insult.data.text }}</h1>
+  <h1>you {{ modifiers.data[m].text }} {{ nouns.data[n].text }}</h1>
 </template>
 
 <style lang="scss" scoped>
