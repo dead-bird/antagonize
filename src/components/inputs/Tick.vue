ca96f1468d2e3be058c4dc3ed3de9c72aee407e0
DaveP <dave@wearecrystal.uk>
2018-04-20 13:04:50 +0100

    Tick component

---
+++ src/components/inputs/Tick.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+};
+</script>
+
+<template>
+  <div class="delete" @click="$emit('save')">✅</div>
+</template>
+
+<style scoped>
+.delete {
+  display: inline-block;
+  user-select: none;
+  font-size: 20px;
+  cursor: pointer;
+  line-height: 1;
+  margin-bottom: 0;
+  padding-top: 1px;
+}
+</style>
