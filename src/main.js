01abe3b69c01fed19922e0c4ddb793212d9e7772
DaveP <dave@wearecrystal.uk>
2018-04-25 15:49:20 +0100

    Added syntax highlighter for fancy examples

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueHighlightJS from 'vue-highlightjs';
 import Antagonize from './Antagonize';
 import router from './router';
+
+Vue.use(VueHighlightJS);
 
 Vue.config.productionTip = false;
 
