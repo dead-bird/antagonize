2238ea6c9f3129735af3bd48131099036dd856cd
DaveP <dave@wearecrystal.uk>
2018-04-25 15:50:03 +0100

    Define component

---
+++ src/components/parts/Define.vue
@@ -0,0 +1,25 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="define">
+    <!-- <h1>antagonize</h1> -->
+    <h4>ænˈtæɡəˌnaɪz</h4>
+    verb<br>
+    <strong>to make hostile; annoy or irritate.</strong><br>
+    "His criticism <em>antagonized</em> his friends"
+  </div>
+</template>
+
+<style scoped>
+.define {
+  position: absolute;
+  bottom: 60px;
+  right: 60px;
+  transform: scale(1.5);
+  transform-origin: bottom right;
+  color: #595959;
+  z-index: -1;
+}
+</style>
