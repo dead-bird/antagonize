1a2778f102102f3d49035acde781444a34bc38d2
DaveP <dave@wearecrystal.uk>
2019-04-16 23:16:51 +0100

    Auth helper file

---
+++ api/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const secret = 'secret';
+
+module.exports = function(bearer) {
+  return new Promise((resolve, reject) => {
+    if (!bearer || bearer.split(' ')[0] !== 'Bearer') {
+      reject('Invalid authentication method.');
+    }
+
+    jwt.verify(bearer.split(' ')[1], secret, (err, decoded) => {
+      if (err) return reject();
+
+      resolve(decoded.id);
+    });
+  });
+};
