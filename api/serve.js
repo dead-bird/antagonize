94c8995f11d83c44a6d6471da342c4f54389d8b0
DaveP <dave@wearecrystal.uk>
2019-04-18 23:44:34 +0100

    Accept Auth headers

--- api/serve.js
+++ api/serve.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Authorization, Accept'
   );
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
